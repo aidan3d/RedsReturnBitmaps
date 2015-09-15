ac8b2b070de7a076a91fdf6e1c954fbce671df9d
aidan3d <aidanhegarty@yahoo.com>
2015-09-15 13:15:58 -0400

    A little tidying up.

--- src/edu/mass/necc/processing/Main.java
+++ src/edu/mass/necc/processing/Main.java
@@ -1,7 +1,7 @@
 //************************************************************
-//             R E D ' S   R E T U R N                       *
+//                R E D ' S   R E T U R N                    *
 //                                                           *
-// Author:     Aidan Hegarty                                 *
+// Author:        Aidan Hegarty                              *
 //                                                           *
 // Last modified: September 15, 2015                         *
 //                                                           *
@@ -10,15 +10,13 @@ package edu.mass.necc.processing;
 
 
 /**
- *
- * @author ahegarty
+ * The Main class is needed to use the <i>Processing</i>
+ * engine in <i>NetBeans</i>.
  */
-public class Main
-{
+public class Main {
     // The Red's Return application starts here. It will be ported to
-    // processing at a later date.
-    public static void main(String[] args)
-    {
+    // the Processing language at a later date.
+    public static void main(String[] args) {
         // Fire up the fully qualified class for what-will-be
         // our Processing "sketch."
         RedsReturnBitmaps.main("edu.mass.necc.processing.RedsReturnBitmaps");
--- src/edu/mass/necc/processing/RedsReturnBitmaps.java
+++ src/edu/mass/necc/processing/RedsReturnBitmaps.java
@@ -7,31 +7,44 @@ package edu.mass.necc.processing;
 
 import processing.core.PApplet;   // Used by the Processing language after port
 /**
- *
  * The RedsReturnBitmaps class will become
  * "RedsReturnBitmaps.pde" when we manually
- * export this project to the Processing language.
+ * port this project to the Processing language.
+ * We are using NetBeans for Java instead of
+ * the Processing ide for the enhanced tools
+ * and debugging power.
  */
-public class RedsReturnBitmaps extends PApplet
-{
+public class RedsReturnBitmaps extends PApplet {
+    /**
+     * This method is home to the Processing
+     * language's size() method
+     */
     @Override
-    public void settings()
-    {
-        // Create the game window.
-        size(360, 480);
+    public void settings(){
+        // create the game window
+        size( 360, 480 );
     }
     
+    /**
+     * All initial housekeeping/setup is done
+     * in this method. It is also needed by
+     * the Processing engine.
+     * .
+     */
     @Override
-    public void setup()
-    {
+    public void setup() {
+        // throw a little gray paint on the canvas
+        background( 125, 125, 125 );
+        
+        // run at a slower 30 frames per second to allow for redraws, etc
+        frameRate(30);
     }
 
+    /**
+     * This method is run every frame by the
+     * Processing language's animation "engine."
+     */
     @Override
-    public void draw()
-    {
-        // Draw a rectangle at the mouse pointer's
-        // position in the game window.
-        rect(mouseX,mouseY, 20, 20);
+    public void draw() {
     }
-
 }
